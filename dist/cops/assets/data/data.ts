aae07de4e6cf069511583176f4b22ad007658348
Kona <konas01@ca.com>
2018-09-17 22:25:37 +0530

    first commit

---
+++ dist/cops/assets/data/data.ts
@@ -0,0 +1,104 @@
+export var single = [
+  // {
+  //   "name": "Germany",
+  //   "value": 8940000
+  // },
+  // {
+  //   "name": "USA",
+  //   "value": 5000000
+  // },
+  // {
+  //   "name": "France",
+  //   "value": 7200000
+  // }
+];
+
+export var multi = [
+  {
+    "name": "Actual Cost",
+    "series": [
+      {
+        "name": "january",
+        "value": 2200
+      },
+      {
+        "name": "febuary",
+        "value": 2700
+      },
+      {
+        "name": "march",
+        "value": 1950
+      },
+      {
+        "name": "april",
+        "value": 1500
+      },
+      {
+        "name": "may",
+        "value": 4300
+      },
+      {
+        "name": "june",
+        "value": 5100
+      }
+    ]
+  },
+  {
+    "name": "Attained Cost",
+    "series": [
+      {
+        "name": "january",
+        "value": 1800
+      },
+      {
+        "name": "febuary",
+        "value": 2100
+      },
+      {
+        "name": "march",
+        "value": 1250
+      },
+      {
+        "name": "april",
+        "value": 3100
+      },
+      {
+        "name": "may",
+        "value": 4500
+      },
+      {
+        "name": "june",
+        "value": 4700
+      }
+    ]
+  },
+  {
+    "name": "Project Cost",
+    "series": [
+      {
+        "name": "january",
+        "value": 2500
+      },
+      {
+        "name": "febuary",
+        "value": 2900
+      },
+      {
+        "name": "march",
+        "value": 1150
+      },
+      {
+        "name": "april",
+        "value": 3900
+      },
+      {
+        "name": "may",
+        "value": 4200
+      },
+      {
+        "name": "june",
+        "value": 5200
+      }
+    ]
+  }
+];
