7741195965535806fa8d33c1435ddad4a82993ca
Kona <konas01@ca.com>
2018-10-10 16:02:11 +0530

    download report option added

---
+++ dist/cops/assets/custom-colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  alert:'rgba(235, 160, 71, 0.7)',
+  warn:'rgba(217, 53, 74, 0.7)',
+  success:'rgba(107, 162, 123, 0.7)',
+  normal:'rgba(54, 162, 235, 0.7)',
+  accent:'',
+  primary:''
+}
+
+export default colors;
